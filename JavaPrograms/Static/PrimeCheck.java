2cada4e7f2584bb25749c489cd6bfb1bce844f7c
Jagadesh Vana <jagadeshvanaoffical@gmail.com>
2025-08-11 17:16:38 +0530

    Added

--- JavaPrograms/Static/PrimeCheck.java
+++ JavaPrograms/Static/PrimeCheck.java
@@ -7,13 +7,12 @@ public class PrimeCheck {
 	{
 		return false;
 	}
-	for(int i = 2 ; i<=Math.sqrt(number); i++)
+	for(int i = 2 ; i <= Math.sqrt(number); i++)
 	{
 		if(number % i ==0)
 		{
 			return false;
-		}
-		
+		}	
 	}
 	return false;
 	}
